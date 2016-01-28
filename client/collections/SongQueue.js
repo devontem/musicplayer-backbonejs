3103be4bb1956fd6046e04a6539e9fc09b7adf82
Devonte M <devontem@live.com>
2016-01-27 22:04:35 -0800

    Init

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,29 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+
+  },
+  
+  playFirst: function(){
+    if (this.models.length) {
+      
+    }
+  },
+
+  add: function(song){
+    // this.playFirst();
+    // console.log('before', this);
+    this.models.push(new SongModel(song));
+    // console.log(this.models);
+    // console.log('after', this);
   }
+  
+  // dequeue: function(){
+    
+  // },
+  
+  // ended: function(){
+    
+  // }
 
 });
